d00ffed65fc6ffdd407520dbb98dd87620ccb453
OGUTAO <lfsantosdeabreu@gmail.com>
2025-07-07 19:22:39 -0300

    v1.0

--- public/js/main.js
+++ public/js/main.js
@@ -88,9 +88,13 @@ document.addEventListener('DOMContentLoaded', function () {
         setupLogoutButtons();
     };
     
-    atualizarNavbar();
-    setupLogoutButtons();
-
+        // CHAMA A FUNÇÃO PARA ATUALIZAR O NAVBAR APENAS UMA VEZ NA PÁGINA INICIAL
+        // NAS OUTRAS PÁGINAS, O CÓDIGO HTML VAI CHAMAR ESSA FUNÇÃO
+        if (window.location.pathname === '/' || window.location.pathname.endsWith('index.html')) {
+            atualizarNavbar();
+        } else {
+            setupLogoutButtons(); // Configura o logout para o caso de o menu já estar no cache
+        }
     const setupPasswordToggle = () => {
         const togglePassword = document.getElementById('togglePassword');
         const passwordInput = document.getElementById('password');
